be20ae2e456fe91dee4b6cfa9231f1ad83c7c25f
JonhSanAvil <jonatan93art@gmail.com>
2022-07-20 19:36:30 +0200

    initial project setup

---
+++ Tema_2/Opcional/app.js
@@ -0,0 +1,47 @@
+
+var hoteles = {
+  Mencey: {
+    name: "Mencey",
+    location: "Tenerife",
+    img:
+      "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/2f/e9/d7/iberostar-grand-mencey.jpg",
+  },
+  Lani: {
+    name: "Lani's Suites Deluxe",
+    location: "Lanzarote",
+    img:
+      "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/49/47/e3/lani-s-suites-deluxe.jpg",
+  },
+};
+
+var selectedHotel = prompt(
+  "Indique hotel sobre el que quiere hacer la reseña: Mencey o Lani",
+);
+
+/* Antonio necesito que me explique como javascript hace que prompt al hacer la pregunta y escribir Mency o Lani conecte con
+dichas variables y el papel que juegan los [] de selectedHotel */
+
+document.getElementById("name-hotel").innerHTML =
+  "Hotel " + hoteles[selectedHotel].name;
+
+document.getElementById("location-hotel").innerHTML =
+  "Ubicado en " + hoteles[selectedHotel].location;
+
+document.getElementById("img-hotel").src = hoteles[selectedHotel].img;
+
+var stars = {
+  una:
+    "<span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
+  dos:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
+  tres:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span>",
+  cuatro:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span>",
+  cinco:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>",
+};
+
+var rating = prompt("Puntuación: una, dos, tres, cuatro o cinco estrellas");
+
+document.getElementById("rating").innerHTML = stars[rating];
