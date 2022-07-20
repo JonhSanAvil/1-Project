a7c578a4bb07312e013bbd37afc78f83e00a2a7d
JonhSanAvil <jonatan93art@gmail.com>
2022-07-20 18:07:52 +0200

    initial project setup

--- Tema_2/consulta.js
+++ Tema_2/consulta.js
@@ -1,26 +1,22 @@
-
-
-var puntuacion = prompt("Puntuación del 1 al 5");
-document.getElementById("rating").innerHTML = puntuacion + " " + "estrellas";
-
-var aceptaCondiciones = confirm("¿Desea que su reseña sea anonima?");
-document.getElementById("anonymous").innerHTML = aceptaCondiciones;
-/* alert(aceptaCondiciones); */
-
-
-
-
 var hotel = {
     name: "NH Málga",
     location: "Málaga",
-    img:"https://static.logitravel.com/cloudcontent/fotos/agregadorHotelero/0000/04518/4518/1.jpg?f=15847051",
-}
+    img:
+    "https://multimedia.andalucia.org/fotos/image_130424.jpeg",
+};
 
-hotel.name 
-document.getElementById("hotel-name").innerHTML = hotel.name;
+document.getElementById("hotel-name").innerHTML = "Hotel " + hotel.name;
+document.getElementById("hotel-location").innerHTML =
+  "Ubicado en " + hotel.location;
+document.getElementById("hotel-img").src = hotel.img;
 
-hotel.location  
-document.getElementById("hotel-location").innerHTML = hotel.location;
 
-hotel.img 
-document.getElementById("hotel-img").innerHTML = hotel.img;
+var puntuacion = prompt("Puntuación del 1 al 5");
+document.getElementById("rating").innerHTML = puntuacion + " estrellas";
+
+var aceptaCondiciones = confirm("¿Desea que su reseña sea anonima?");
+document.getElementById("anonymous").checked = aceptaCondiciones;
+/* Preguntar por checked y innerHTML funciones entiendo que checked es para marcar
+el check pero innerHTML ¿? */
+
+
